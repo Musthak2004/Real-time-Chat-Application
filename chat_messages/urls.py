bfc7a51925df369ed5d8bf228d7a6595b5f09ce7
Musthak <musthakcool55@gmail.com>
2026-06-29 09:16:09 +0530

    Fix chat_messages views: add participant permission check and get_object_or_404

--- chat_messages/urls.py
+++ chat_messages/urls.py
@@ -1,4 +1,14 @@
 from django.urls import path
 
+from .views import MessageCreateView
+
+app_name = "chat_messages"
+
 urlpatterns = [
+
+    path(
+        "send/<int:conversation_id>/",
+        MessageCreateView.as_view(),
+        name="send_message"
+    ),
 ]
--- chat_messages/views.py
+++ chat_messages/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from chat.models import Conversation
+
+from .forms import MessageForm
+from .models import Message
+
+
+class MessageCreateView(
+    LoginRequiredMixin,
+    CreateView
+):
+
+    model = Message
+
+    form_class = MessageForm
+
+    template_name = "chat_messages/message_form.html"
+
+    def form_valid(self, form):
+
+        conversation = get_object_or_404(
+            Conversation,
+            pk=self.kwargs["conversation_id"],
+            participants=self.request.user,
+        )
+
+        form.instance.sender = self.request.user
+
+        form.instance.conversation = conversation
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+
+        return reverse_lazy(
+            "chat:conversation_detail",
+            kwargs={
+                "pk": self.kwargs["conversation_id"]
+            }
+        )
