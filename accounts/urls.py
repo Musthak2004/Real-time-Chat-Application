c0eb9eb3e7032d1a856558ae83b23df6ab384fee
Musthak <musthakcool55@gmail.com>
2026-06-29 07:18:48 +0530

    Fix views, templates, URL namespaces for accounts app

---
+++ accounts/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+
+from .views import (
+    SignUpView,
+    ProfileUpdateView,
+)
+
+app_name = "accounts"
+
+urlpatterns = [
+
+    path(
+        "signup/",
+        SignUpView.as_view(),
+        name="signup"
+    ),
+
+    path(
+        "profile/",
+        ProfileUpdateView.as_view(),
+        name="profile"
+    ),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+)
 
-# Create your views here.
+from .forms import (
+    SignUpForm,
+    ProfileUpdateForm,
+)
+
+from .models import CustomUser
+
+
+class SignUpView(CreateView):
+
+    model = CustomUser
+
+    form_class = SignUpForm
+
+    template_name = "registration/signup.html"
+
+    success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+
+        response = super().form_valid(form)
+
+        login(
+            self.request,
+            self.object
+        )
+
+        return response
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+
+    model = CustomUser
+
+    form_class = ProfileUpdateForm
+
+    template_name = "accounts/profile_update.html"
+
+    success_url = reverse_lazy("home")
+
+    def get_object(self):
+
+        return self.request.user
